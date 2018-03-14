0394e089250e168218d9a2c098f31bf9e3880414
Jennifer Morris <j2leigh@gmail.com>
2018-03-13 21:18:49 -0500

    login call and api
    Setup login call.

---
+++ js/sc/access.js
@@ -0,0 +1,22 @@
+/*Temp API Server*/
+
+var express = require('express');
+var app = express();
+var fs = require("fs");
+
+app.get('/user', function (req, res) {
+   fs.readFile("grantAccess.json", 'utf8', function (err, data) {
+       console.log( data );
+       res.end( data );
+   });
+})
+
+var server = app.listen(8081, function () {
+
+  var host = server.address().address
+  var port = server.address().port
+
+  console.log("Example app listening at http://%s:%s", host, port)
+
+})
+
