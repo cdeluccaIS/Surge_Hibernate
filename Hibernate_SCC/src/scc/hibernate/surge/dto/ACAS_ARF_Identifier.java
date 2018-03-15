54dc69c5031999929e0ec52bbabf613c03181fc6
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-03-15 17:39:44 -0400

    added one-to-one annotation between ACAS_ARF_Identifier and ACAS_ARF_ID_FQDN

--- Hibernate_SCC/src/scc/hibernate/surge/dto/ACAS_ARF_Identifier.java
+++ Hibernate_SCC/src/scc/hibernate/surge/dto/ACAS_ARF_Identifier.java
@@ -1,27 +1,33 @@
 package scc.hibernate.surge.dto;
 
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity (name="ACAS_ARF_Identifier")
 public class ACAS_ARF_Identifier{
 	@Id
 	@Column (name="uuid")
 	private UUID uuid;
-	@Column (name="FQDN")
-	private UUID fqdn;
+	@OneToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@JoinColumn(name="FQDN")
+	private ACAS_ARF_ID_FQDN fqdn;
+	
 	public UUID getUuid() {
 		return uuid;
 	}
 	public void setUuid(UUID uuid) {
 		this.uuid = uuid;
 	}
-	public UUID getFqdn() {
+	public ACAS_ARF_ID_FQDN getFqdn() {
 		return fqdn;
 	}
-	public void setFqdn(UUID fqdn) {
+	public void setFqdn(ACAS_ARF_ID_FQDN fqdn) {
 		this.fqdn = fqdn;
 	}
 	
