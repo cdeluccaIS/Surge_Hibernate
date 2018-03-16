7f2a810b573408febb39e100a5448d2638b9eac6
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-03-16 08:54:31 -0400

    added one-to-one annotation between ACAS_ARF_ReportObject and  ACAS_ARF_Device

--- Hibernate_SCC/src/scc/hibernate/surge/dto/ACAS_ARF_ReportObject.java
+++ Hibernate_SCC/src/scc/hibernate/surge/dto/ACAS_ARF_ReportObject.java
@@ -1,27 +1,33 @@
 package scc.hibernate.surge.dto;
 
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 @Entity (name="ACAS_ARF_ReportObject")
 public class ACAS_ARF_ReportObject {
 	@Id
 	@Column (name="uuid")
 	private UUID uuid;
-	@Column (name="device")
-	private UUID device;
+	@OneToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@JoinColumn(name="device")
+	private ACAS_ARF_Device device;
+	
 	public UUID getUuid() {
 		return uuid;
 	}
 	public void setUuid(UUID uuid) {
 		this.uuid = uuid;
 	}
-	public UUID getDevice() {
+	public ACAS_ARF_Device getDevice() {
 		return device;
 	}
-	public void setDevice(UUID device) {
+	public void setDevice(ACAS_ARF_Device device) {
 		this.device = device;
 	}
 }
