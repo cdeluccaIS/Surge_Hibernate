d091b9bfe6583b646805b562fffbe4ace328d72a
sukanyaJoy <31313790+sukanyaJoy@users.noreply.github.com>
2018-03-16 09:07:56 -0400

    added many-to-one annotation between ACAS_ARF_Config ACAS_ARF_NETCONFIG, ACAS_ARF_cpeRecord

--- Hibernate_SCC/src/scc/hibernate/surge/dto/ACAS_ARF_Config.java
+++ Hibernate_SCC/src/scc/hibernate/surge/dto/ACAS_ARF_Config.java
@@ -1,35 +1,42 @@
 package scc.hibernate.surge.dto;
 
 import java.util.UUID;
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
 
 @Entity (name="ACAS_ARF_Config")
 public class ACAS_ARF_Config{
 	@Id
 	@Column (name="uuid")
 	private UUID uuid;
-	@Column (name="networkConfig")
-	private UUID networkConfig;
-	@Column (name="cpeInv")
-	private UUID cpeInv;
+	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@JoinColumn(name="networkConfig")
+	private ACAS_ARF_NETCONFIG networkConfig;
+	@ManyToOne(cascade=CascadeType.ALL, fetch=FetchType.EAGER)
+	@JoinColumn(name="cpeInv")
+	private ACAS_ARF_cpeRecord cpeInv;
+	
 	public UUID getUuid() {
 		return uuid;
 	}
 	public void setUuid(UUID uuid) {
 		this.uuid = uuid;
 	}
-	public UUID getNetworkConfig() {
+	public ACAS_ARF_NETCONFIG getNetworkConfig() {
 		return networkConfig;
 	}
-	public void setNetworkConfig(UUID networkConfig) {
+	public void setNetworkConfig(ACAS_ARF_NETCONFIG networkConfig) {
 		this.networkConfig = networkConfig;
 	}
-	public UUID getCpeInv() {
+	public ACAS_ARF_cpeRecord getCpeInv() {
 		return cpeInv;
 	}
-	public void setCpeInv(UUID cpeInv) {
+	public void setCpeInv(ACAS_ARF_cpeRecord cpeInv) {
 		this.cpeInv = cpeInv;
 	}
 }
